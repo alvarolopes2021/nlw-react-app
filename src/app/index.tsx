d6d714d8d0a8d66fb16cf95c6ec6e5d4480a9bc6
alvaro lopes <alvarolopes2020@protonmail.com>
2024-02-15 23:16:00 -0300

    BREAKING CHANGE: create project

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Home() {
+    return (
+        <View className="flex-1 pt-8">
+            <Text className='text-white text-xl'>WIZARDS!!!</Text>
+        </View>
+    );
+}
